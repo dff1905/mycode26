8795740d513de8a44edb84c3624a502121a2b190
dff1905 <63223937+dff1905@users.noreply.github.com>
2020-04-06 12:55:37 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,25 @@
+
+package com.tutorial;
+
+public class main {
+
+    public static void main(String[] args){
+
+        //    ini buat bikin baris pake println(dia enter) dan berurutan ke bawah
+        System.out.println("hello world");
+        System.out.println("hello indonesia");
+
+        System.out.print("halo semua");
+        System.out.println("haiii");
+
+//        bisa pake spasi manual dengan menulis \n
+        System.out.print("mantapp\n");
+        System.out.print("mantull\n");
+        System.out.println("manjing\n");
+
+//      bisa juga ngeluarin angka pake printf
+
+        System.out.printf("naga wiro sableng %d", 212);
+
+    }
+}
